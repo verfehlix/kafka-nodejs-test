0bd8110de56c5819ecf13ac9f1f4ed4eab9c34b9
Felix M <felix.p.magnus@gmail.com>
2019-04-08 14:19:15 +0200

    encode / decode all sent messages via people schema

--- consumer.js
+++ consumer.js
@@ -1,4 +1,10 @@
+const fs = require('fs');
 const kafka = require('kafka-node');
+const avroSchemaRregistry = require('avro-schema-registry');
+
+const registry = avroSchemaRregistry('http://localhost:29081');
+const schemaFileData = fs.readFileSync('peopleSchema.json', 'utf8');
+const schema = JSON.parse(schemaFileData);
 
 const client = new kafka.KafkaClient({
   kafkaHost: 'localhost:29092',
@@ -8,8 +14,17 @@ const consumer = new kafka.Consumer(client, [{ topic: 'people', parition: 0 }], 
   autoCommit: false,
 });
 
-consumer.on('message', (msg) => {
-  console.log(msg);
+async function decodePersonMessage(personMessage) {
+  const messageValue = personMessage.value;
+  const messageBuffer = Buffer.from(messageValue, 'utf8');
+  const decodedPerson = await registry.decode(messageBuffer);
+
+  return decodedPerson;
+}
+
+consumer.on('message', async (msg) => {
+  const decodedMessage = await decodePersonMessage(msg);
+  console.log(decodedMessage);
 });
 
 consumer.on('error', (error) => {
--- producer.js
+++ producer.js
@@ -13,57 +13,63 @@ const registry = avroSchemaRregistry('http://localhost:29081');
 const schemaFileData = fs.readFileSync('peopleSchema.json', 'utf8');
 const schema = JSON.parse(schemaFileData);
 
-const fakePerson = {
-  firstName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
-  birthDate: faker.date.past().toLocaleDateString(),
-};
+// registry
+//   .encodeMessage('topic', schema, fakePerson)
+//   .then((msg) => {
+//     console.log(msg); // <Buffer 00 00 00 00 01 18 74 65 73 74 20 6d 65 73 73 61 67 65>
 
-registry
-  .encodeMessage('topic', schema, fakePerson)
-  .then((msg) => {
-    console.log(msg); // <Buffer 00 00 00 00 01 18 74 65 73 74 20 6d 65 73 73 61 67 65>
-
-    return registry.decode(msg);
-  })
-  .then((msg) => {
-    console.log(msg); // test message
-  });
-
-// const producer = new kafka.Producer(client);
-
-// function startSending() {
-//   const fakePerson = {
-//     firstName: faker.name.firstName(),
-//     lastName: faker.name.lastName(),
-//     birthDate: faker.date.past(),
-//   };
-
-//   const payloads = [
-//     {
-//       topic: 'people',
-//       messages: [JSON.stringify(fakePerson)],
-//       partition: 0,
-//     },
-//   ];
-//   producer.send(payloads, (err, data) => {
-//     if (err) {
-//       console.log('ERROR');
-//       console.log(err);
-//     } else {
-//       console.log(data);
-//     }
+//     return registry.decode(msg);
+//   })
+//   .then((msg) => {
+//     console.log(msg); // test message
 //   });
-// }
 
-// producer.on('ready', () => {
-//   console.log('Starting the sending...');
+async function encodePersonBySchema(person) {
+  console.log('encoding person by schema...');
 
-//   setInterval(startSending, 2000);
-// });
+  const encodedPerson = await registry.encodeMessage('people', schema, person);
 
-// producer.on('error', (error) => {
-//   console.log('Error during setup of producer:');
+  console.log('done encoding.');
 
-//   console.log(error);
-// });
+  return encodedPerson;
+}
+
+const producer = new kafka.Producer(client);
+
+async function startSending() {
+  const fakePerson = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    birthDate: faker.date.past().toLocaleDateString(),
+  };
+
+  const payload = await encodePersonBySchema(fakePerson);
+
+  const payloads = [
+    {
+      topic: 'people',
+      messages: [payload],
+      partition: 0,
+    },
+  ];
+  producer.send(payloads, (err, data) => {
+    if (err) {
+      console.log('ERROR');
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+producer.on('ready', () => {
+  console.log('Starting the sending...');
+
+  setInterval(startSending, 2000);
+});
+
+producer.on('error', (error) => {
+  console.log('Error during setup of producer:');
+
+  console.log(error);
+});
